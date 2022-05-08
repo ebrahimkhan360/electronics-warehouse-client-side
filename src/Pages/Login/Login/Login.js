b3d8f7dd848435427c183c299254bddd98e61e50
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-05-08 16:12:46 +0600

    Login and register funtion add

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,26 +1,80 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    if (loading || sending) {
+        return <Loading></Loading>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error.message}</p>
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password);
+    }
+    const navigateRegister = e => {
+        navigate('/register');
+    }
+
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Sent email');
+        }
+        else {
+            toast('please enter your email address');
+        }
+    }
     return (
         <div className='container text-center w-50 mx-auto'>
             <h2 className='text-primary text-center my-2'>Please Login: </h2>
-            <form>
-                <input className='w-50' type="email" name="email" id="" placeholder='Email Address' required />
+            <form onSubmit={handleSubmit}>
+                <input className='w-50' ref={emailRef} type="email" name="email" id="" placeholder='Email Address' required />
                 <br />
                 <br />
-                <input className='w-50' type="password" name="password" id="" placeholder='Password' required />
+                <input className='w-50' ref={passwordRef} type="password" name="password" id="" placeholder='Password' required />
                 <br />
                 <input
                     className='w-50 mx-auto btn btn-primary mt-2'
                     type="submit"
                     value="Login" />
             </form>
-
-            {/* <p>You New User? <Link to="/register" className='text-info pe-auto text-decoration-none' >Please Register</Link></p>
-            <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none'>Reset Password</button></p> */}
-            {/* <Spinner></Spinner> */}
-            {/* <SocialLogin></SocialLogin> */}
+            {errorElement}
+            <p>You New User? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
+            <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button></p>
+            <SocialLogin></SocialLogin>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
